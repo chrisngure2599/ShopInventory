edff415b10a4ae3dc135441e650a19531a7bc0bb
Chris Ngure <chrisngure2599@gmail.com>
2021-11-29 11:24:20 +0300

    Update 2021_11_29_132732_fix_discount_error_on_sales_details.php

--- database/migrations/2021_11_29_132732_fix_discount_error_on_sales_details.php
+++ database/migrations/2021_11_29_132732_fix_discount_error_on_sales_details.php
@@ -18,7 +18,7 @@ class FixDiscountErrorOnSalesDetails extends Migration
             DB::statement('ALTER TABLE sales_details MODIFY COLUMN discount_amount DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
             DB::statement('ALTER TABLE sales_details MODIFY COLUMN tax_percent DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
 
-            DB::statement('ALTER TABLE sales_details MODIFY COLUMN tax_amouunt DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN tax_amount DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
             DB::statement('ALTER TABLE sales_details MODIFY COLUMN grand_total DOUBLE(50,2)');
             DB::statement('ALTER TABLE sales_details MODIFY COLUMN closing_balance DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
             DB::statement('ALTER TABLE sales_details MODIFY COLUMN closing_due DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
