bea4e3438f6d7b68969941dc7cc64d1a3949f7f1
Chris Ngure <chrisngure2599@gmail.com>
2021-11-29 11:16:28 +0300

    Create 2021_11_29_132732_fix_discount_error_on_sales_details.php

---
+++ database/migrations/2021_11_29_132732_fix_discount_error_on_sales_details.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixDiscountErrorOnSalesDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales_details',function(){
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN discount_percent DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN discount_amount DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN tax_percent DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN tax_amouunt DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN grand_total DOUBLE(50,2)');
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN closing_balance DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+            DB::statement('ALTER TABLE sales_details MODIFY COLUMN closing_due DOUBLE(50,2) UNSIGNED NOT NULL DEFAULT "0.00"');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
